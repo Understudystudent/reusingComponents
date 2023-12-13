b83b4118a17bf935e71f0392213ad3024cd75631
Understudystudent <carlmjames2@gmail.com>
2023-12-13 15:39:57 +0200

    done

---
+++ src/components/Product.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <h2>Child Component</h2>
+        <p>{{ greeting }}</p>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'ProductComp',
+        props: ['greeting'],
+        components: {
+        }
+    }
+</script>
+<!-- Local styling if you make use of scoped -->
+<style scoped>
+.container {
+    background-color: darkgreen;
+    color: white;
+}
+</style>
+
+
+
+
